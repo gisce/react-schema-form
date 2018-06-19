22a6c21321c2ab04ed9664d305c3eed76047545f
Xavi Torelló <info@xaviertorello.cat>
2018-06-19 17:42:29 +0200

    Improve Binary field to use a drag'n'drop zone
    It uses react-dropzone to render a draggable zone to handle the
    insertion of files.
    
    If no selected files, show a description. Else, render the list of
    selected files.
    
    It handles new properties:
    - `mime_types`: list of accepted mime_types or extensions (i.e
    "*.png"). By default permit all files.
    - `authorized_file_message`: label to show if dragged file is correct
    - `unauthorized_file_message`: label to show if dragged file is not
    correct
    - `description`: node to embed inside the dropzone if no file is
    selected

--- src/Binary.js
+++ src/Binary.js
@@ -3,16 +3,29 @@
  */
 import React from 'react';
 import ComposedComponent from './ComposedComponent';
-import RaisedButton from 'material-ui/RaisedButton';
 import FileIcon from 'material-ui/svg-icons/editor/attach-file';
+import Dropzone from 'react-dropzone';
 
+const styles = {
+    dropzone: {
+        borderStyle: "dashed",
+        borderRadius: "10px",
+        padding: "10px",
+        borderWidth: "2px",
+    }
+}
 
 class Binary extends React.Component {
+    constructor() {
+      super()
+      this.files = [];
+    }
 
-    handleUpdate(selectorFiles: FileList)
-    {
+    handleUpdate = (selectorFiles: FileList) => {
+        this.files = Object.assign([], this.files, selectorFiles)
+
         const {key, type} = this.props.form
-        const file_attachment = selectorFiles[0];
+        const file_attachment = selectorFiles;
 
         return this.props.onChange(key, file_attachment, type, this.props.form)
     }
@@ -20,26 +33,44 @@ class Binary extends React.Component {
     render() {
         // console.log('Binary', this.props);
 
-        return (
-            <div className={this.props.form.htmlClass}>
-                <RaisedButton
-                    containerElement='label'
-                    disabled={this.props.form.readonly}
-                    // errorText={this.props.error}
-                    label={this.props.form.placeholder || "Puja un fitxer"}
-                    style={this.props.form.style || {width: '100%'}}
-                >
-                    <FileIcon/>
-                    <input
-                        type="file"
-                        // accept="image/*"
-                        // style={{"display":"none"}}
-                        onChange={ (e) => this.handleUpdate(e.target.files) }
-                        label={this.props.form.title || "Upload a file"}
-                    />
-                </RaisedButton>
-
+        const description = (
+            <div>
+                <p><FileIcon/></p>
+                <p>Drag some files here, or click to select files to upload.</p>
             </div>
+        );
+
+        // default all mime
+        const mime_types = [];
+
+        return (
+          <section>
+            <div className="dropzone">
+              <h4>{this.props.form.title || "Upload a file"}</h4>
+
+              <Dropzone
+                disabled={this.props.form.readonly}
+                accept={this.props.form.mime_types || mime_types}
+                style={{...styles.dropzone, ...this.props.form.style}}
+                onDrop={(accepted, rejected) => { this.handleUpdate(accepted) }}
+              >
+
+                {({ isDragActive, isDragReject, acceptedFiles, rejectedFiles }) => {
+                  if (isDragActive) {
+                    return this.props.form.authorized_file_message || "This file is authorized";
+                  }
+                  if (isDragReject) {
+                    return this.props.form.unauthorized_file_message || "Unauthorized file type";
+                  }
+
+                  return acceptedFiles.length?
+                  		acceptedFiles.map(f => <li>{f.name}</li>)
+                        :
+                        this.props.description || description;
+                }}
+              </Dropzone>
+            </div>
+          </section>
         );
     }
 }
